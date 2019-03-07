8760fd0d14242ad217acc68d4874ddc18fbd83bd
Joseph Kohlmann <kohlmannj@mac.com>
2019-03-07 10:19:10 -0500

    Let's use `let`!

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Get our "clickable" elements
-var clickableElements = document.querySelectorAll('.clickable');
+let clickableElements = document.querySelectorAll('.clickable');
 
-for (var i = 0; i < clickableElements.length; i++) {
-  var el = clickableElements[i];
-  var text = el.textContent;
+for (let i = 0; i < clickableElements.length; i++) {
+  let el = clickableElements[i];
+  let text = el.textContent;
 
   el.addEventListener('click', function() {
     // Example Output: `Element 1 reads, "Foo"`
