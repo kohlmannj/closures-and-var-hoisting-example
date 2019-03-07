62861c1367861c064f5c1f2eb07dc30c4846372f
Joseph Kohlmann <kohlmannj@mac.com>
2019-03-07 10:20:26 -0500

    Use `let index` inside loop to avoid IE 11 issue

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // Get our "clickable" elements
 let clickableElements = document.querySelectorAll('.clickable');
 
-for (let i = 0; i < clickableElements.length; i++) {
+for (var i = 0; i < clickableElements.length; i++) {
+  let index = i;
   let el = clickableElements[i];
   let text = el.textContent;
 
   el.addEventListener('click', function() {
     // Example Output: `Element 1 reads, "Foo"`
-    console.log('Element ' + i + ' reads, "' + text + '"');
+    console.log('Element ' + index + ' reads, "' + text + '"');
   });
 }
