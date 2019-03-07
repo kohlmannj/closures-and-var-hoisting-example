40e1f8e22188990f31149ac86afe9d6c45caf9a2
Joseph Kohlmann <kohlmannj@mac.com>
2019-03-07 10:40:49 -0500

    2nd bonus: use forEach(), but it’s no-go in IE 11

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 // Get our "clickable" elements
 var clickableElements = document.querySelectorAll('.clickable');
 
-for (var i = 0; i < clickableElements.length; i++) {
-  (function() {
-    var index = i;
-    var el = clickableElements[i];
-    var text = el.textContent;
+// CAVEAT: clickableElements is a NodeList,
+// and IE 11 doesn't support `NodeList.forEach()`!
+/** @see https://developer.mozilla.org/en-US/docs/Web/API/NodeList#Browser_compatibility */
+clickableElements.forEach(function(el, i) {
+  var text = el.textContent;
 
-    el.addEventListener('click', function() {
-      // Example Output: `Element 1 reads, "Foo"`
-      console.log('Element ' + index + ' reads, "' + text + '"');
-    });
-  })();
-}
+  el.addEventListener('click', function() {
+    // Example Output: `Element 1 reads, "Foo"`
+    console.log('Element ' + i + ' reads, "' + text + '"');
+  });
+});
